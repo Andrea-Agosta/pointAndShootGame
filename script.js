6ce3ebf862ad8c715fd3e305d7bb629fd3d459a2
Andrea-Agosta <andrea.agosta85@gmail.com>
2022-09-03 16:59:42 +0200

    refactoring code

--- script.js
+++ script.js
@@ -124,18 +124,18 @@ class Particle {
 }
 
 function drawScore(){
-    ctx.fillStyle = "black";
-    ctx.fillText('Score: ' + score, 50,75);
     ctx.fillStyle = "white";
+    ctx.fillText('Score: ' + score, 50,75);
+    ctx.fillStyle = "black";
     ctx.fillText('Score: ' + score, 55,80);
 }
 
 function drawGameOver(){
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
-    ctx.fillText('GAME OVER, your score is: ' + score, canvas.width/2, canvas.height/2);
+    ctx.fillText('GAME OVER your score is: ' + score, canvas.width/2, canvas.height/2);
     ctx.fillStyle = "white";
-    ctx.fillText('GAME OVER, your score is: ' + score, canvas.width/2 + 5, canvas.height/2 + 5);
+    ctx.fillText('GAME OVER your score is: ' + score, canvas.width/2 + 5, canvas.height/2 + 5);
 }
 
 window.addEventListener('click', function (e){
